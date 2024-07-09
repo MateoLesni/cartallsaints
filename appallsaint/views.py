6673c3ff660ba8c342528a9485bc8a2204ed0206
MateoLesni <mateolesniewicz@gmail.com>
2024-07-09 09:15:59 -0300

    Add .env and credentials.json to .gitignore, remove credentials from history

--- appallsaint/views.py
+++ appallsaint/views.py
@@ -1,15 +1,25 @@
+import os
 from django.shortcuts import render
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 
 def menu_view(request):
     # Configura el alcance de la API y las credenciales
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('appallsaint/credentials/credentials.json', scope)
+    
+    # Leer la ruta del archivo de credenciales desde la variable de entorno
+    credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+    # Cargar las credenciales desde el archivo JSON
+    with open(credentials_path, 'r') as f:
+        credentials_dict = json.load(f)
+    
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
     client = gspread.authorize(credentials)
 
     # Abre la hoja de cálculo deseada usando la clave de la hoja (sheet key)
-    sheet = client.open_by_key('1SDNLkmf_lIPtR777XG_MjLTcJjY8EqaU8IV2hnMju3k').sheet1  # Reemplaza 'TU_SHEET_KEY' con la clave de tu hoja de cálculo
+    sheet = client.open_by_key('1SDNLkmf_lIPtR777XG_MjLTcJjY8EqaU8IV2hnMju3k').sheet1
 
     # Obtén todos los registros de la hoja
     records = sheet.get_all_records()
@@ -17,7 +27,7 @@ def menu_view(request):
     # Procesa los datos de Google Sheets como items del menú
     categories = {}
     for record in records:
-        category = record['Categoria']  # Asegúrate de que los nombres de las columnas coincidan con los de tu hoja
+        category = record['Categoria']
         item = {
             'name': record['Nombre'],
             'description': record['Descripción'],
